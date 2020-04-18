b2c0669dc43fdd3eb047a24274f422d7acb35510
Adam A <adam.adj@protonmail.com>
2020-04-18 11:46:23 -0400

    Upgrades to code

--- messenger/messenger-fix.ts
+++ messenger/messenger-fix.ts
@@ -1,16 +1,20 @@
 (() => {
     const uselessDivSelector = 'div[style="left: 9007200000000000px; width: 100%; height: 100%; position: fixed;"';
-    const foundUselessDiv = uselessDiv => interval => {
+    const intervalTime = 100;
+    let interval: number;
+
+    const foundUselessDiv = (uselessDiv: Element) => (interval: number) => {
         uselessDiv.setAttribute('style', '');
         clearInterval(interval);
+
         console.log('Found and fixed div, ending');
     }
-    const intervalTime = 10;
-    let interval: number;
+
     try {
+        console.log('Messenger detected, attempting to clear buggy div tag');
         interval = setInterval(() => {
             const uselessDiv = document.querySelector(uselessDivSelector);
-            uselessDiv ? foundUselessDiv(uselessDiv)(interval) : console.log('div not found...');
+            uselessDiv ? foundUselessDiv(uselessDiv)(interval) : null;
         }, intervalTime);
     } catch (error) {
         console.log(error);

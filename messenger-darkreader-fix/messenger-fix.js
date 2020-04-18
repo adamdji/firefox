05798a8c72c3f98e1f0633497fd27518eb33303d
Adam A <adam.adj@protonmail.com>
2020-04-18 12:43:39 -0400

    Surrounded JS with IFFE

--- messenger-darkreader-fix/messenger-fix.js
+++ messenger-darkreader-fix/messenger-fix.js
@@ -1,18 +1,20 @@
-const uselessDivSelector = 'div[style="left: 9007200000000000px; width: 100%; height: 100%; position: fixed;"';
-const intervalTime = 100;
-let interval;
-const foundUselessDiv = (uselessDiv) => (interval) => {
-    uselessDiv.setAttribute('style', '');
-    clearInterval(interval);
-    console.log('Found and fixed div, ending');
-};
-try {
-    console.log('Messenger detected, attempting to clear buggy div tag');
-    interval = setInterval(() => {
-        const uselessDiv = document.querySelector(uselessDivSelector);
-        uselessDiv ? foundUselessDiv(uselessDiv)(interval) : null;
-    }, intervalTime);
-}
-catch (error) {
-    console.log(error);
-}
+(() => {
+    const uselessDivSelector = 'div[style="left: 9007200000000000px; width: 100%; height: 100%; position: fixed;"';
+    const intervalTime = 100;
+    let interval;
+    const foundUselessDiv = (uselessDiv) => (interval) => {
+        uselessDiv.setAttribute('style', '');
+        clearInterval(interval);
+        console.log('Found and fixed div, ending');
+    };
+    try {
+        console.log('Messenger detected, attempting to clear buggy div tag');
+        interval = setInterval(() => {
+            const uselessDiv = document.querySelector(uselessDivSelector);
+            uselessDiv ? foundUselessDiv(uselessDiv)(interval) : null;
+        }, intervalTime);
+    }
+    catch (error) {
+        console.log(error);
+    }
+})();

d8a7f849273ea5670a5ae9d022caaa2faed25ac5
Adam A <adam.adj@protonmail.com>
2020-04-18 12:01:45 -0400

    Working extension

--- messenger/messenger-fix.js
+++ messenger/messenger-fix.js
@@ -1,19 +1,18 @@
-(function () {
-    var uselessDivSelector = 'div[style="left: 9007200000000000px; width: 100%; height: 100%; position: fixed;"';
-    var foundUselessDiv = function (uselessDiv) { return function (interval) {
-        uselessDiv.setAttribute('style', '');
-        clearInterval(interval);
-        console.log('Found and fixed div, ending');
-    }; };
-    var intervalTime = 10;
-    var interval;
-    try {
-        interval = setInterval(function () {
-            var uselessDiv = document.querySelector(uselessDivSelector);
-            uselessDiv ? foundUselessDiv(uselessDiv)(interval) : console.log('div not found...');
-        }, intervalTime);
-    }
-    catch (error) {
-        console.log(error);
-    }
-})();
+const uselessDivSelector = 'div[style="left: 9007200000000000px; width: 100%; height: 100%; position: fixed;"';
+const intervalTime = 100;
+let interval;
+const foundUselessDiv = (uselessDiv) => (interval) => {
+    uselessDiv.setAttribute('style', '');
+    clearInterval(interval);
+    console.log('Found and fixed div, ending');
+};
+try {
+    console.log('Messenger detected, attempting to clear buggy div tag');
+    interval = setInterval(() => {
+        const uselessDiv = document.querySelector(uselessDivSelector);
+        uselessDiv ? foundUselessDiv(uselessDiv)(interval) : null;
+    }, intervalTime);
+}
+catch (error) {
+    console.log(error);
+}
--- messenger/messenger-fix.ts
+++ messenger/messenger-fix.ts
@@ -1,22 +1,20 @@
-(() => {
-    const uselessDivSelector = 'div[style="left: 9007200000000000px; width: 100%; height: 100%; position: fixed;"';
-    const intervalTime = 100;
-    let interval: number;
+const uselessDivSelector = 'div[style="left: 9007200000000000px; width: 100%; height: 100%; position: fixed;"';
+const intervalTime = 100;
+let interval: number;
 
-    const foundUselessDiv = (uselessDiv: Element) => (interval: number) => {
-        uselessDiv.setAttribute('style', '');
-        clearInterval(interval);
+const foundUselessDiv = (uselessDiv: Element) => (interval: number) => {
+    uselessDiv.setAttribute('style', '');
+    clearInterval(interval);
 
-        console.log('Found and fixed div, ending');
-    }
+    console.log('Found and fixed div, ending');
+}
 
-    try {
-        console.log('Messenger detected, attempting to clear buggy div tag');
-        interval = setInterval(() => {
-            const uselessDiv = document.querySelector(uselessDivSelector);
-            uselessDiv ? foundUselessDiv(uselessDiv)(interval) : null;
-        }, intervalTime);
-    } catch (error) {
-        console.log(error);
-    }
-})();
+try {
+    console.log('Messenger detected, attempting to clear buggy div tag');
+    interval = setInterval(() => {
+        const uselessDiv = document.querySelector(uselessDivSelector);
+        uselessDiv ? foundUselessDiv(uselessDiv)(interval) : null;
+    }, intervalTime);
+} catch (error) {
+    console.log(error);
+}

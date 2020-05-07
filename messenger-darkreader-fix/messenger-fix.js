ac8c53e5fbe1ab1643c2a75af388f65b42c421f0
Adam <adam.adj@protonmail.com>
2020-05-07 15:30:09 -0400

    Finish upgrading to version 2
    -targetting ES5
    -Leaner code
    -Source map

--- messenger-darkreader-fix/messenger-fix.js
+++ messenger-darkreader-fix/messenger-fix.js
@@ -1,22 +1,14 @@
-(() => {
-    // body > div:nth-child(148) is CSS-Selector from browser
-    const uselessDivSelector = 'div[style="left: 9007200000000000px; width: 100%; height: 100%; position: fixed;"';
-    const intervalTime = 100;
-    let interval;
-    const foundUselessDiv = (uselessDiv) => (interval) => {
-        uselessDiv.setAttribute('style', '');
-        clearInterval(interval);
-        console.log('Found and fixed div, ending');
-    };
-    try {
-        console.log('Messenger detected, attempting to clear buggy div tag');
-        interval = setInterval(() => {
-            const uselessDiv = document.querySelector(uselessDivSelector);
-            uselessDiv ? foundUselessDiv(uselessDiv)(interval) : null;
-        }, intervalTime);
-    }
-    catch (error) {
-        console.log(error);
-    }
-})();
+new MutationObserver(function (mutationList, observer) {
+    mutationList.forEach(function (mutation) {
+        mutation.addedNodes.forEach(function (node) {
+            if (node.nodeName === 'DIV' && node.nodeType === Node.ELEMENT_NODE) {
+                var foundDiv = node;
+                if (foundDiv.style.left === '9007200000000000px') {
+                    foundDiv.setAttribute('style', '');
+                    observer.disconnect();
+                }
+            }
+        });
+    });
+}).observe(document.querySelector('body'), { attributes: false, childList: true, subtree: false, attributeOldValue: false, characterDataOldValue: false });
 //# sourceMappingURL=messenger-fix.js.map
--- messenger-darkreader-fix/messenger-fix.ts
+++ messenger-darkreader-fix/messenger-fix.ts
@@ -1,30 +1,13 @@
-(() => {
-    // body > div:nth-child(148) is CSS-Selector from browser
-    const uselessDivSelector = 'body > div[style="left: 9007200000000000px; width: 100%; height: 100%; position: fixed;"';
-    const intervalTime = 100;
-    let interval: number;
-
-    const foundUselessDiv = (uselessDiv: Element) => (interval: number) => {
-        uselessDiv.setAttribute('style', '');
-        clearInterval(interval);
-
-        console.log('Found and fixed div, ending');
-    }
-    try {
-        console.log('Messenger detected, attempting to clear buggy div tag');
-        interval = setInterval(() => {
-            const uselessDiv = document.querySelector(uselessDivSelector);
-            uselessDiv ? foundUselessDiv(uselessDiv)(interval) : null;
-        }, intervalTime);
-    } catch (error) {
-        console.log(error);
-    }
-})();
-
-(() => {
-    const uselessDivSelector: string = 'body > div[style="left: 9007200000000000px; width: 100%; height: 100%; position: fixed;"';
-    const body: HTMLBodyElement = document.querySelector('body');
-
-    
-    const observerConfig: object = { attributes: true, childList: true, subtree: true };
-})();
+new MutationObserver((mutationList: MutationRecord[], observer: MutationObserver): void => {
+    mutationList.forEach(mutation => {
+        mutation.addedNodes.forEach(node => {
+            if (node.nodeName === 'DIV' && node.nodeType === Node.ELEMENT_NODE) {
+                const foundDiv: HTMLDivElement = node as HTMLDivElement;
+                if (foundDiv.style.left === '9007200000000000px') {
+                    foundDiv.setAttribute('style', '');
+                    observer.disconnect();
+                }
+            }
+        });
+    });
+}).observe(document.querySelector('body'), { attributes: false, childList: true, subtree: false, attributeOldValue: false, characterDataOldValue: false });
